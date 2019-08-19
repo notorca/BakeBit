dc3e670d944cf4d5d132b1408df47b7869b2bdb6
notorca <notorca@gmail.com>
2019-08-19 19:08:37 +0200

    New script implementation

--- Software/Python/bakebit_nanohat_oled.py
+++ Software/Python/bakebit_nanohat_oled.py
@@ -41,59 +41,212 @@ from PIL import ImageDraw
 import time
 import sys
 import subprocess
-import threading
+import Queue
 import signal
 import os
 import socket
 import fcntl
 import struct
+import ctypes
 
-global width
-width=128
-global height
-height=64
+width=oled.SeeedOLED_Width
+height=oled.SeeedOLED_Height
+screenUpdateTime = 1
 
-global pageCount
-pageCount=2
-global pageIndex
-pageIndex=0
-global showPageIndicator
-showPageIndicator=False
 
-global pageSleep
-pageSleep=120
-global pageSleepCountdown
-pageSleepCountdown=pageSleep
-
-oled.init()  #initialze SEEED OLED display
-oled.setNormalDisplay()      #Set display to normal mode (i.e non-inverse mode)
-oled.setHorizontalMode()
-
-global drawing
-drawing = False
-
-global image
 image = Image.new('1', (width, height))
-global draw
 draw = ImageDraw.Draw(image)
-global fontb24
 fontb24 = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 24)
-global font14
 font14 = ImageFont.truetype('DejaVuSansMono.ttf', 14)
-#Added
-global font12
 font12 = ImageFont.truetype('DejaVuSansMono.ttf', 12)
-global smartFont
 smartFont = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 10)
-global font10
 font10 = ImageFont.truetype('DejaVuSansMono.ttf', 10)
-global fontb14
 fontb14 = ImageFont.truetype('DejaVuSansMono-Bold.ttf', 14)
-global font11
 font11 = ImageFont.truetype('DejaVuSansMono.ttf', 11)
 
-global lock
-lock = threading.Lock()
+libc = ctypes.CDLL('libc.so.6')
+uptimeBuf = ctypes.create_string_buffer(4096) # generous buffer to hold struct sysinfo
+
+
+class View:
+    def render(self, draw):
+        pass
+    def processEvent(self, event):
+        return self
+
+class VnstatView(View):
+    def render(self, draw):
+        try:
+            IPAddress = get_ip_address('eth0')
+        except:
+            IPAddress = get_ip()
+        draw.text((2,2),"IP:" + str(IPAddress),font=font12, fill=255)
+        #This months usage
+        cmd = "vnstat -m 2 -i eth0 | grep \"$(date +'%Y-%m')\" | awk '{print $8\" \"substr ($9, 1, 3)}'"
+        #cmd = "vnstat -m 2 -i eth0 | grep \"2019-09\" | awk '{print $8\" \"substr ($9, 1, 3)}'" #Usage for this month
+        strCurrentUsage = subprocess.check_output(cmd, shell = True )
+        draw.text((2,30),"Usage: " + strCurrentUsage, font=font12, fill=255)
+        #Last months usage
+        cmd = "vnstat -m 2 -i eth0 | grep \"$(date --date=\"$(date +%Y-%m-15) -1 month\" +'%Y-%m')\" | awk '{print $8\" \"substr ($9, 1, 3)}'"
+        #cmd = "vnstat -m 2 -i eth0 | grep \"2019-08\" | awk '{print $8\" \"substr ($9, 1, 3)}'" #Usage for this month
+        strCurrentUsage = subprocess.check_output(cmd, shell = True )
+        draw.text((2,45),"Last: " + strCurrentUsage, font=font12, fill=255)
+
+    def processEvent(self, event):
+        return {
+            'K1': lambda: self,
+            'K2': StatsView,
+            'K3': RebootView,
+        }[event]()         
+
+class StatsView(View):
+    def render(self, draw):
+        # Draw some shapes.
+        # First define some constants to allow easy resizing of shapes.
+        padding = 2
+        top = padding
+        # Move left to right keeping track of the current x position for drawing shapes.
+        x = 0
+        #Get CPU Load Averages
+        #cmd = "uptime | cut -d : -f 4" #This option fails after a while
+        #cmd = "uptime | awk '{print $9, $10, $11}'"
+        #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        cmd = "uptime | awk '{if ($4 == \"min,\") print $9, $10, $11; else print $8, $9, $10;}'"
+        CPU = subprocess.check_output(cmd, shell = True )
+        #Get system uptime
+        cmd = "uptime -p | cut -c3-" #1 hour, x minutes
+        UpTime = subprocess.check_output(cmd, shell = True )
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell = True )
+        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+        Disk = subprocess.check_output(cmd, shell = True )
+        tempI = int(open('/sys/class/thermal/thermal_zone0/temp').read())
+        if tempI>1000:
+            tempI = tempI/1000
+        tempStr = "Temperature: %sC" % str(tempI)
+
+        draw.text((x, top+1),    "CPU:" + str(CPU), font=font10, fill=255)
+        #draw.text((x, top+5+12),    str(CPU), font=smartFont, fill=255)
+        draw.text((x, top+5+8),         "Up: " + str(UpTime), font=font10, fill=255)
+        draw.text((x, top+5+20),    str(MemUsage),  font=font10, fill=255)
+        draw.text((x, top+5+32),    str(Disk),  font=font10, fill=255)
+        draw.text((x, top+5+44),    tempStr,  font=font10, fill=255)
+
+    def processEvent(self, event):
+        return {
+            'K1': VnstatView,
+            'K2': lambda: self,
+            'K3': RebootView,
+        }[event]()    
+
+class RebootView(View):
+    def __init__(self):
+        self._selected = 1
+
+    def render(self, draw):
+        draw.text((2, 2),  'Exit',  font=fontb14, fill=255)
+
+        draw.rectangle((2,20,width-4,20+16), outline=0, fill=(255 if self._selected == 1 else 0))
+        draw.text((4, 22),  'Shutdown',  font=font11, fill=(0 if self._selected == 1 else 255))
+
+        draw.rectangle((2,38,width-4,38+16), outline=0, fill=(255 if self._selected == 2 else 0))
+        draw.text((4, 40),  'Reboot',  font=font11, fill=(0 if self._selected == 2 else 255))
+
+    def processEvent(self, event):
+        return {
+            'K1': self._next,  # move
+            'K2': lambda: ConfirmationView('Shutdown' if self._selected == 1 else 'Reboot'),  # confirm
+            'K3': VnstatView,
+        }[event]()  
+
+    def _next(self):
+        self._selected = 2 if self._selected == 1 else 1
+        return self
+
+class ConfirmationView(View):
+    def __init__(self, title):
+        self._title = title
+        self._selected = 1
+
+    def render(self, draw):
+        draw.text((2, 2), self._title + '?', font=fontb14, fill=255)
+
+        draw.rectangle((2,20,width-4,20+16), outline=0, fill=(255 if self._selected == 1 else 0))
+        draw.text((4, 22),  'Yes',  font=font11, fill=(0 if self._selected == 1 else 255))
+
+        draw.rectangle((2,38,width-4,38+16), outline=0, fill=(255 if self._selected == 2 else 0))
+        draw.text((4, 40),  'No',  font=font11, fill=(0 if self._selected == 2 else 255))
+
+    def processEvent(self, event):
+        return {
+            'K1': self._next,  # move
+            'K2': VnstatView if self._selected == 2 else lambda: ByeView('poweroff' if self._title == 'Shutdown' else 'reboot'),  # confirm
+            'K3': VnstatView,
+        }[event]()
+
+    def _next(self):
+        self._selected = 2 if self._selected == 1 else 1
+        return self
+
+class ByeView(View):
+    def __init__(self, action):
+        self._action = action
+        self._end = False
+    
+    def render(self, draw):
+        if self._end:
+            os.system('systemctl ' + self._action)
+            raise KeyboardInterrupt
+        
+        self._end = True
+        draw.text((2, 2),  'Performing ' + self._action,  font=fontb14, fill=255)
+        draw.text((2, 20),  'Please wait',  font=font11, fill=255)
+
+eventQueue = Queue.Queue()
+
+def receive_signal(signum, stack):
+    if signum == signal.SIGUSR1:
+        key = 'K1'
+    elif signum == signal.SIGUSR2:
+        key = 'K2'
+    elif signum == signal.SIGALRM:
+        key = 'K3'
+    print(key, 'pressed')
+    eventQueue.put(key)
+
+def main():
+    oled.init()  #initialze SEEED OLED display
+    oled.setNormalDisplay()      #Set display to normal mode (i.e non-inverse mode)
+    oled.setHorizontalMode()
+
+    oled.drawImage(Image.open('friendllyelec.png').convert('1'))
+    time.sleep(1)
+
+    signal.signal(signal.SIGUSR1, receive_signal)
+    signal.signal(signal.SIGUSR2, receive_signal)
+    signal.signal(signal.SIGALRM, receive_signal)
+
+    currentView = VnstatView()
+
+    while True:
+        try:
+            try:
+                event = eventQueue.get(True, screenUpdateTime)
+                currentView = currentView.processEvent(event)
+            except Queue.Empty:
+                pass
+
+            # Draw a black filled box to clear the image
+            draw.rectangle((0,0,width,height), outline=0, fill=0)
+            currentView.render(draw)
+            oled.drawImage(image)
+        except KeyboardInterrupt:
+            break
+        except IOError as e:
+            print ("Error:", e)
+
+    # Cleanup before exit
+    oled.clearDisplay()
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -115,256 +268,13 @@ def get_ip():
         s.close()
     return IP
 
-def draw_page():
-    global drawing
-    global image
-    global draw
-    global oled
-    global font14
-    global smartFont
-    global width
-    global height
-    global pageCount
-    global pageIndex
-    global showPageIndicator
-    global width
-    global height
-    global lock
-    global pageSleepCountdown
+def uptime():
+    if libc.sysinfo(uptimeBuf) != 0:
+        print('failed')
+        return -1
 
-    lock.acquire()
-    is_drawing = drawing
-    page_index = pageIndex
-    lock.release()
+    uptime = struct.unpack_from('@l', uptimeBuf.raw)[0]
+    return uptime
 
-    if is_drawing:
-        return
-
-    #if the countdown is zero we should be sleeping (blank the display to reduce screenburn)
-    if pageSleepCountdown == 1:
-        oled.clearDisplay()
-        pageSleepCountdown = pageSleepCountdown - 1
-        return
-
-    if pageSleepCountdown == 0:
-        return
-
-    pageSleepCountdown = pageSleepCountdown - 1
-
-    lock.acquire()
-    drawing = True
-    lock.release()
-
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    # Draw current page indicator
-    if showPageIndicator:
-        dotWidth=4
-        dotPadding=2
-        dotX=width-dotWidth-1
-        dotTop=(height-pageCount*dotWidth-(pageCount-1)*dotPadding)/2
-        for i in range(pageCount):
-            if i==page_index:
-                draw.rectangle((dotX, dotTop, dotX+dotWidth, dotTop+dotWidth), outline=255, fill=255)
-            else:
-                draw.rectangle((dotX, dotTop, dotX+dotWidth, dotTop+dotWidth), outline=255, fill=0)
-            dotTop=dotTop+dotWidth+dotPadding
-
-    if page_index==0:
-        try:
-            IPAddress = get_ip_address('eth0')
-        except:
-            IPAddress = get_ip()
-
-        draw.text((2,2),"IP:" + str(IPAddress),font=font12, fill=255)
-
-
-        #draw.text((2,16), "Data Statistics:", font=font12, fill=255)
-        #This months usage
-        cmd = "vnstat -m 2 -i eth0 | grep \"$(date +'%Y-%m')\" | awk '{print $8\" \"substr ($9, 1, 3)}'"
-        #cmd = "vnstat -m 2 -i eth0 | grep \"2019-09\" | awk '{print $8\" \"substr ($9, 1, 3)}'" #Usage for this month
-        strCurrentUsage = subprocess.check_output(cmd, shell = True )
-        draw.text((2,30),"Usage: " + strCurrentUsage, font=font12, fill=255)
-
-        #Last months usage
-        cmd = "vnstat -m 2 -i eth0 | grep \"$(date --date=\"$(date +%Y-%m-15) -1 month\" +'%Y-%m')\" | awk '{print $8\" \"substr ($9, 1, 3)}'"
-        #cmd = "vnstat -m 2 -i eth0 | grep \"2019-08\" | awk '{print $8\" \"substr ($9, 1, 3)}'" #Usage for this month
-        strCurrentUsage = subprocess.check_output(cmd, shell = True )
-        draw.text((2,45),"Last: " + strCurrentUsage, font=font12, fill=255)
-
-        #text = time.strftime("%A")
-        #draw.text((2,2),text,font=font14,fill=255)
-        #text = time.strftime("%e %b %Y")
-        #draw.text((2,18),text,font=font14,fill=255)
-        #text = time.strftime("%X")
-        #draw.text((2,40),text,font=fontb24,fill=255)
-    elif page_index==1:
-        # Draw some shapes.
-        # First define some constants to allow easy resizing of shapes.
-        padding = 2
-        top = padding
-        # Move left to right keeping track of the current x position for drawing shapes.
-        x = 0
-        try:
-            IPAddress = get_ip_address('eth0')
-        except:
-            IPAddress = get_ip()
-        #Get CPU Load Averages
-        #cmd = "uptime | cut -d : -f 4" #This option fails after a while
-        #cmd = "uptime | awk '{print $9, $10, $11}'"
-        #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-        cmd = "uptime | awk '{if ($4 == \"min,\") print $9, $10, $11; else print $8, $9, $10;}'"
-        CPU = subprocess.check_output(cmd, shell = True )
-        #Get system uptime
-        cmd = "uptime -p | cut -c3-" #1 hour, x minutes
-        UpTime = subprocess.check_output(cmd, shell = True )
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-        MemUsage = subprocess.check_output(cmd, shell = True )
-        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-        Disk = subprocess.check_output(cmd, shell = True )
-        tempI = int(open('/sys/class/thermal/thermal_zone0/temp').read())
-        if tempI>1000:
-            tempI = tempI/1000
-        tempStr = "Temperature: %sC" % str(tempI)
-
-        #draw.text((x, top+5),       "IP: " + str(IPAddress),  font=smartFont, fill=255)
-        draw.text((x, top+1),    "CPU:" + str(CPU), font=font10, fill=255)
-        #draw.text((x, top+5+12),    str(CPU), font=smartFont, fill=255)
-        draw.text((x, top+5+8),         "Up: " + str(UpTime), font=font10, fill=255)
-        draw.text((x, top+5+20),    str(MemUsage),  font=font10, fill=255)
-        draw.text((x, top+5+32),    str(Disk),  font=font10, fill=255)
-        draw.text((x, top+5+44),    tempStr,  font=font10, fill=255)
-    elif page_index==3: #shutdown -- no
-        draw.text((2, 2),  'Shutdown?',  font=fontb14, fill=255)
-
-        draw.rectangle((2,20,width-4,20+16), outline=0, fill=0)
-        draw.text((4, 22),  'Yes',  font=font11, fill=255)
-
-        draw.rectangle((2,38,width-4,38+16), outline=0, fill=255)
-        draw.text((4, 40),  'No',  font=font11, fill=0)
-
-    elif page_index==4: #shutdown -- yes
-        draw.text((2, 2),  'Shutdown?',  font=fontb14, fill=255)
-
-        draw.rectangle((2,20,width-4,20+16), outline=0, fill=255)
-        draw.text((4, 22),  'Yes',  font=font11, fill=0)
-
-        draw.rectangle((2,38,width-4,38+16), outline=0, fill=0)
-        draw.text((4, 40),  'No',  font=font11, fill=255)
-
-    elif page_index==5:
-        draw.text((2, 2),  'Shutting down',  font=fontb14, fill=255)
-        draw.text((2, 20),  'Please wait',  font=font11, fill=255)
-
-    oled.drawImage(image)
-
-    lock.acquire()
-    drawing = False
-    lock.release()
-
-
-def is_showing_power_msgbox():
-    global pageIndex
-    lock.acquire()
-    page_index = pageIndex
-    lock.release()
-    if page_index==3 or page_index==4:
-        return True
-    return False
-
-
-def update_page_index(pi):
-    global pageIndex
-    lock.acquire()
-    pageIndex = pi
-    lock.release()
-
-def receive_signal(signum, stack):
-    global pageIndex
-    global pageSleepCountdown
-    global pageSleep
-
-    pageSleepCountdown = pageSleep #user pressed a button, reset the sleep counter
-
-    lock.acquire()
-    page_index = pageIndex
-    lock.release()
-
-    if page_index==5:
-        return
-
-    if signum == signal.SIGUSR1:
-        print('K1 pressed')
-        if is_showing_power_msgbox():
-            if page_index==3:
-                update_page_index(4)
-            else:
-                update_page_index(3)
-            draw_page()
-        else:
-            pageIndex=0
-            draw_page()
-
-    if signum == signal.SIGUSR2:
-        print('K2 pressed')
-        if is_showing_power_msgbox():
-            if page_index==4:
-                update_page_index(5)
-                draw_page()
-
-            else:
-                update_page_index(0)
-                draw_page()
-        else:
-            update_page_index(1)
-            draw_page()
-
-    if signum == signal.SIGALRM:
-        print('K3 pressed')
-        if is_showing_power_msgbox():
-            update_page_index(0)
-            draw_page()
-        else:
-            update_page_index(3)
-            draw_page()
-
-
-image0 = Image.open('friendllyelec.png').convert('1')
-oled.drawImage(image0)
-time.sleep(2)
-
-signal.signal(signal.SIGUSR1, receive_signal)
-signal.signal(signal.SIGUSR2, receive_signal)
-signal.signal(signal.SIGALRM, receive_signal)
-
-while True:
-    try:
-        draw_page()
-
-        lock.acquire()
-        page_index = pageIndex
-        lock.release()
-
-        if page_index==5:
-            time.sleep(2)
-            while True:
-                lock.acquire()
-                is_drawing = drawing
-                lock.release()
-                if not is_drawing:
-                    lock.acquire()
-                    drawing = True
-                    lock.release()
-                    oled.clearDisplay()
-                    break
-                else:
-                    time.sleep(.1)
-                    continue
-            time.sleep(1)
-            os.system('systemctl poweroff')
-            break
-        time.sleep(1)
-    except KeyboardInterrupt:
-        break
-    except IOError:
-        print ("Error")
+if __name__ == "__main__":
+    main()
